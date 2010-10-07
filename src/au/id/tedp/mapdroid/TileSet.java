b9861061e972f60a2581e93b6a62cb12af11c1fb
Ted Percival <ted@tedp.id.au>
2010-10-07 11:32:44 -0600

    Apply GNU Lesser General Public License

--- src/au/id/tedp/mapdroid/TileSet.java
+++ src/au/id/tedp/mapdroid/TileSet.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Mapdroid.
+ * Copyright 2009 Ted Percival <ted@tedp.id.au>.
+ *
+ * Mapdroid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Mapdroid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Mapdroid. If not, see <http://www.gnu.org/licenses/>.
+ */
 // TileSet: A single style of tile images that may be used side-by-side to
 // render a map.
 package au.id.tedp.mapdroid;
